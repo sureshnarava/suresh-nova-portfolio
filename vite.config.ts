eb4505760328f4a439090995949ce47496730112
sureshnarava <sureshbabunarava@gmail.com>
2025-09-01 23:31:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/suresh-portfolio/',
+  base: "/suresh-portfolio/",
   server: {
     host: "::",
     port: 8080,
